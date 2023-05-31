d9eb1026e30fb27d081cde850e250bf7964c0658
Sidimar Salla <85910112+Sidimar-Salla@users.noreply.github.com>
2023-05-31 00:18:40 -0300

    Inserção de daods aleaatorios

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 
-from recipes import views
+from . import views
 
-# Create your views here.
 urlpatterns = [
     path('', views.home),
-    path('recipes/<int:id>/', views.recipe)
+    path('recipes/<int:id>/', views.recipe),
 ]
-
